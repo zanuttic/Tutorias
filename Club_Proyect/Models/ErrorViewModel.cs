bb27b64c7f7d4edeba630e37023e904f0bc467ac
Carlos Zanutti <carlos.zanutti@gmail.com>
2020-09-24 14:22:35 -0300

    Primer Commit
    Primer commit de inicio de proyecto

---
+++ Club_Proyect/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Club_Proyect.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
