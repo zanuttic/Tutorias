f2a549e60be21ab0f6a6f5c07352d649e5e9d271
Carlos Zanutti <carlos.zanutti@gmail.com>
2020-10-02 14:21:11 -0300

    Clase Vecino

---
+++ Club_Proyect/Club_Proyect/Entities/Vecino.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Club_Proyect.Entities
+{
+    public class Vecino
+    {
+        public int Id { get; set; }
+    }
+}
