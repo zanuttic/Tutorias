fcba97d460a59c0b684e187afd459e78566c4bbc
Carlos Zanutti <carlos.zanutti@gmail.com>
2020-10-02 14:30:24 -0300

    desde el .rar

---
+++ Club_Proyect/Club_Proyect/Entities/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Club_Proyect.Entity
+{
+    public class Persona 
+    {
+        [Key]
+        public int DNI { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public string Direccion { get; set; }
+
+        public bool Activo { get; set; }
+
+
+
+    }
+}
---
+++ Club_Proyect/Club_Proyect/Entities/Socio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Club_Proyect.Entity
+{
+    public class Socio : Persona
+    {
+
+        [Key]
+        public int NumSocio { get; set; }
+
+        public DateTime FechaIngresoClub { get; set; }
+
+        public int Categoria { get; set; }
+
+        public bool ActivoOno { get; set; }
+
+         
+
+    }
+}
