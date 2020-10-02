d706c2e436b7e536a40055dd5e1bb5cf9b2fd386
Ivan Kopech <ivankopech@hotmail.com>
2020-10-02 14:39:43 -0300

    Clases cliente y empleado

---
+++ Club_Proyect/Club_Proyect/Entities/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Club_Proyect.Entity
+{
+    public class Cliente : Persona
+    {
+        [Key]
+        public int Num_Cliente { get; set; }
+        public double Saldo { get; set; }
+        public bool Activo_oNo { get; set; }
+    }
+}
---
+++ Club_Proyect/Club_Proyect/Entities/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Club_Proyect.Entity
+{
+    public class Empleado : Persona
+    {
+        [Key]
+        public int Num_Legajo { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public string Sector { get; set; }
+        public bool Activo_oNo { get; set; }
+    }
+}
