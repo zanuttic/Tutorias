2aa9c4cdfa9c03e7122e18429a68db00e3fae034
Carlos Zanutti <carlos.zanutti@gmail.com>
2020-10-22 09:55:48 -0300

    codigo ignorado

---
+++ Club_Proyect/Club_Proyect/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Club_Proyect.Entities;
+using Club_Proyect.Entity;
+
+namespace Club_Proyect.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+
+        }
+        public DbSet<Club_Proyect.Entity.Persona> Persona { get; set; }
+        public DbSet<Club_Proyect.Entity.Cliente> Cliente { get; set; }
+
+        //public DbSet<Persona> personas = new DbSet<Perso
+        // public DbSet<Cliente> clientes { get; set; }
+        // public DbSet<Empleado> empleados { get; set; }
+        // public DbSet<Socio> socios { get; set; }
+        // public DbSet<Vecino> vecinos { get; set; }
+
+    }
+}
---
+++ Club_Proyect/Club_Proyect/Entities/Vecino.cs
@@ -0,0 +1,16 @@
+﻿using Club_Proyect.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Club_Proyect.Entities
+{
+    public class Vecino
+    {
+        public Persona persona { get; set; }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string telefono { get; set; }
+    }
+}
